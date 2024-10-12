28d4fb1c886b3a87f521a8f9d37e8cd583e37c8a
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-11 20:42:40 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_security_baca_diaz/AppSecurityBacaDiazApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_security_baca_diaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityBacaDiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSecurityBacaDiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_security_baca_diaz/AppSecurityBacaDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_security_baca_diaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityBacaDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
